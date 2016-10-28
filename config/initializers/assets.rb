80b5d969d952e86c2b7144b1385726cb039315ce
Andres Hernandez <ahernandez@growthaccelerationpartners.com>
2016-10-27 23:29:53 -0600

    Add assets precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,9 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+# CSS
+Rails.application.config.assets.precompile += %w(jquery.dataTables.min.css)
+
+# JS
+Rails.application.config.assets.precompile += %w(jquery.dataTables.min.js)
